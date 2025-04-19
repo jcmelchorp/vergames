9271628ea757a86028aae10f2179ca7fe72ef762
Julio C. Melchor P <jcmelchorp@gmail.com>
2025-04-18 22:54:58 -0600

    Agregar servicio de Snack y mejorar gestión de metadatos; implementar inicialización de título y generación de etiquetas para SEO en el servicio de trabajador web.

--- projects/yousuck/src/app.component.ts
+++ projects/yousuck/src/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ShellComponent } from './app/pages/shell/shell';
+import { WebServiceWorkerService } from './app/pages/service/webservice-worker.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,16 @@ import { ShellComponent } from './app/pages/shell/shell';
   imports: [RouterModule, ShellComponent],
   template: `<prmsc-shell></prmsc-shell> <router-outlet></router-outlet>`,
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Yousuck - Aplicación de nada';
+  constructor(private webServiceWorker: WebServiceWorkerService) {}
+  ngOnInit(): void {
+    this.webServiceWorker.titleInit();
+    this.webServiceWorker.generateTags({
+      title: this.title,
+      description:
+        'Aplicación de nada, pero con un propósito. Yousuck es una aplicación que te ayuda a encontrar tu propósito en la vida.',
+      image: 'screenshot01.png',
+    });
+  }
+}
--- projects/yousuck/src/app/pages/service/webservice-worker.service.ts
+++ projects/yousuck/src/app/pages/service/webservice-worker.service.ts
@@ -1,6 +1,9 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
-import { BehaviorSubject, Subscription, interval } from 'rxjs';
+import { BehaviorSubject, Subscription, filter, interval, map } from 'rxjs';
+import { SnackService } from './snack.service';
+import { Meta, Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +14,14 @@ export class WebServiceWorkerService implements OnDestroy {
   );
   serviceSubscriptions: Subscription[] = [];
 
-  constructor(public swUpdate: SwUpdate) {
+  constructor(
+    private swUpdate: SwUpdate,
+    private snack: SnackService,
+    private titleService: Title,
+    private metaService: Meta,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) {
     this.initialize();
   }
 
@@ -27,6 +37,13 @@ export class WebServiceWorkerService implements OnDestroy {
           console.log(evt);
           if (evt.type === 'VERSION_READY') {
             this.$isAnyNewUpdateAvailable.next(true);
+            this.snack
+              .messageWithReload(
+                'Se han hecho cambios desde la última visita. Actualiza la página para continuar',
+                'Ok',
+              )
+              ?.afterDismissed()
+              .subscribe(() => window.location.reload());
           }
         }),
       );
@@ -45,5 +62,88 @@ export class WebServiceWorkerService implements OnDestroy {
 
   ngOnDestroy(): void {
     this.serviceSubscriptions?.forEach((x) => x?.unsubscribe());
+  }
+
+  titleInit() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          let child = this.activatedRoute.firstChild;
+          while (child) {
+            if (child.firstChild) {
+              child = child.firstChild;
+            } else if (child.snapshot.data && child.snapshot.data['title']) {
+              return child.snapshot.data['title'];
+            } else {
+              return null;
+            }
+          }
+          return null;
+        }),
+      )
+      .subscribe((data: any) => {
+        if (data) {
+          this.titleService.setTitle(data);
+        }
+      });
+  }
+
+  generateTags({ title = '', description = '', image = '' }): void {
+    this.titleService.setTitle(title);
+    this.metaService.addTags([
+      {
+        name: 'keywords',
+        content: 'Yousuck games, Yousuck, games, you suck, you suck games',
+      },
+      { name: 'description', content: description },
+      { name: 'robots', content: 'index, follow' },
+      { charset: 'UTF-8' },
+      { httpEquiv: 'X-UA-Compatible', content: 'IE=edge' },
+      { name: 'author', content: 'Pete Sahatt' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0, shrink-to-fit=no',
+      },
+      { name: 'date', content: '2025-04-18', scheme: 'YYYY-MM-DD' },
+      { name: 'application-name', content: title },
+      { name: 'apple-mobile-web-app-status-bar', content: 'black-translucent' },
+      { name: 'theme-color', content: '#1976d2' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'mobile-web-app-capable', content: 'yes' },
+      { name: 'msapplication-TileColor', content: '#2b5797' },
+      {
+        name: 'msapplication-square70x70logo',
+        content: 'assets/icons/mstile-icon-128.png',
+      },
+      {
+        name: 'msapplication-square150x150logo',
+        content: 'assets/icons/mstile-icon-270.png',
+      },
+      {
+        name: 'msapplication-square310x310logo',
+        content: 'assets/icons/mstile-icon-558.png',
+      },
+      {
+        name: 'msapplication-wide310x150logo',
+        content: 'assets/icons/mstile-icon-558-270.png',
+      },
+      // OpenGraph metatags
+      { property: 'og:title', content: title },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:site_name', content: title },
+      {
+        property: 'og:url',
+        content: 'https://yousuck.web.app',
+      },
+      { property: 'og:image:url', content: image },
+      { property: 'og:image:secure_url', content: image },
+      { property: 'og:image:alt', content: 'Website view example' },
+      { property: 'og:image:type', content: 'image/png' },
+      { property: 'og:description', content: description },
+      { property: 'twitter:card', content: 'summary_large_image' },
+      { property: 'twitter:text:title', content: title },
+      { property: 'twitter:image', content: image },
+    ]);
   }
 }
